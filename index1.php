1eec198d0c17c86c2442216960dc933c0ce0e847
Siddharth Jain <101965057+sidj7@users.noreply.github.com>
2022-07-14 14:40:43 +0530

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,18 @@
+<title>Siddharth jain PHP</title>
+<h1>Siddharth jain PHP</h1>
+<p><?php
+	print "The SHA256 hash of \"siddharth jain\" is ".hash('sha256', 'siddharth jain');
+?></p>
+<pre>ASCII ART:
+	       ***
+	      *****
+	     **	  **
+	    **     **
+	   ***********
+	  *************
+	 **           **
+	**             **
+</pre>
+<a href="fail.php">Click here to check the error setting</a>
+<br>
+<a href="check.php">Click here to cause a traceback</a>
